441fad47251b5eaf880a6a099d3046e73838fd4a
tavttima <github@vattima.com>
2018-09-01 12:32:24 -0400

    Upserts a row into the HangmanGame table, ensuring that only a single row ever exists, updtaing the gameState as necessary
    TECHFEST18-22

--- CreateGame.py
+++ CreateGame.py
@@ -1,32 +1,60 @@
-import logging, uuid, datetime
+import logging, uuid, datetime, boto3, json
+from botocore.exceptions import ClientError
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def handler(event, context):
     logger.info('Handling event {} - context {}', event, context)
 
-    # Get the game from the game table - DynamoDB call
+    # Initial game state
     game = {
-       "state": "over"
+        "gameState": "over"
     }
 
-    # If the game state is 'over', update the game state to 'created'
-    if game['state'] == "over":
-        # generate game id
-        game['id'] = uuid.uuid4()
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    hangmanGame = dynamodb.Table('HangmanGame')
 
+    # Get the game from the game table
+    response = hangmanGame.scan()
+    gameExists = len(response["Items"]) == 1
+    if gameExists:
+        game = response["Items"][0]
+        logger.info('Found existing game record {}', game)
+    else:
+        # generate the first and only game id. Starting a new game will reuse this uuid
+        print("Game does not exist!")
+        game["Id"] = str(uuid.uuid4())
+        logger.info('No existing game record. Creating game id {}', game["Id"])
+
+    # If the game state is "over", update the game state to "created"
+    if game["gameState"] == "over":
         # generate timestamp
-        game['startDatetime'] = datetime.datetime.utcnow().isoformat()
+        game["startDatetime"] = str(datetime.datetime.utcnow().isoformat())
 
         # set state to created
-        game['state'] = 'created'
+        game["gameState"] = 'created'
 
         # set solution to the command
-        game['solution'] = event['data']['command']['arguments'][1]
+        game["solution"] = event["data"]["command"]["arguments"][1]
 
-        # update the game - DynamoDB call
-        event['data']['game'] = game
+        try:
+            # upsert the game - DynamoDB call
+            response = hangmanGame.update_item(
+                Key={
+                    'Id': game["Id"]
+                },
+                UpdateExpression='SET startDateTime = :std, gameState = :st, solution = :sln',
+                ExpressionAttributeValues={
+                    ':std': game["startDatetime"],
+                    ':st': game["gameState"],
+                    ':sln': game["solution"],
+                }
+            )
+        except ClientError as e:
+            print("Unexpected error: %s" % e)
+
+    event["data"]["game"] = game
 
     return event
-
--- GreatGameTests.py
+++ GreatGameTests.py
@@ -1,13 +1,14 @@
-import unittest, json, CreateGame
-
+import unittest, json, CreateGame, boto3
 
 class TestStringMethods(unittest.TestCase):
 
     def test_handlerWithGameOver(self):
+        #Pre-condition - HangmanGame table must be created and empty
         input = self.loadEvent("./test/resources/test-CreateGame-input-01.json")
-        event = CreateGame.handler(input['input'], None)
-        self.assertEqual(event['data']['game']['solution'], 'prototypes')
-        self.assertEqual(event['data']['game']['state'], 'created')
+        event = CreateGame.handler(input["input"], {})
+        print(event)
+        self.assertEqual(event["data"]["game"]["solution"], "prototypes")
+        self.assertEqual(event["data"]["game"]["gameState"], "created")
 
     def loadEvent(self, jsonFilename):
         f = open(jsonFilename)
