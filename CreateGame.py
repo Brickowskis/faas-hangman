28fd6ba04a151a1713738231aaedbf9ef5f68cf1
tavttima <github@vattima.com>
2018-09-03 20:20:48 -0400

    Adds response and errors
    TECHFEST18-22

--- CreateGame.py
+++ CreateGame.py
@@ -8,6 +8,9 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     logger.info('Handling event {} - context {}', event, context)
 
+    responseMessage = event["data"]["response"]["sms"]
+    errors = event["data"]["errors"]
+
     # Initial game state
     game = {
         "gameState": "over"
@@ -22,11 +25,13 @@ def handler(event, context):
     if gameExists:
         game = response["Items"][0]
         logger.info('Found existing game record {}', game)
+        responseMessage = responseMessage + "\n" + "A game record exists"
     else:
         # generate the first and only game id. Starting a new game will reuse this uuid
         print("Game does not exist!")
         game["Id"] = str(uuid.uuid4())
         logger.info('No existing game record. Creating game id {}', game["Id"])
+        responseMessage = responseMessage + "\n" + "A game record does not exist"
 
     # If the game state is "over", update the game state to "created"
     if game["gameState"] == "over":
@@ -45,7 +50,7 @@ def handler(event, context):
                 Key={
                     'Id': game["Id"]
                 },
-                UpdateExpression='SET startDateTime = :std, gameState = :st, solution = :sln',
+                UpdateExpression='SET startDatetime = :std, gameState = :st, solution = :sln',
                 ExpressionAttributeValues={
                     ':std': game["startDatetime"],
                     ':st': game["gameState"],
@@ -53,8 +58,19 @@ def handler(event, context):
                 }
             )
         except ClientError as e:
+            errors = e
+            responseMessage = responseMessage + "\n" + "Error ["+e+"]"
             print("Unexpected error: %s" % e)
 
+    if game["gameState"] == "created":
+        responseMessage = responseMessage + "\n" + "Game created"
+        responseMessage = responseMessage + "\n" + "Id ["+game["Id"]+"]"
+        responseMessage = responseMessage + "\n" + "Start Datetime ["+game["startDatetime"]+"]"
+        responseMessage = responseMessage + "\n" + "Game State ["+game["gameState"]+"]"
+        responseMessage = responseMessage + "\n" + "Solution ["+game["solution"]+"]"
+
+    event["data"]["errors"] = errors
     event["data"]["game"] = game
+    event["data"]["response"]["sms"] = responseMessage
 
     return event
