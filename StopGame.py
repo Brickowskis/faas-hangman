2a41d8e680ecd18f29cda10ed11463100edab067
tavttima <github@vattima.com>
2018-09-03 20:31:05 -0400

    Adds response and errors
    TECHFEST18-32

--- StopGame.py
+++ StopGame.py
@@ -8,6 +8,9 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     logger.info('Handling event {} - context {}', event, context)
 
+    responseMessage = event["data"]["response"]["sms"]
+    errors = event["data"]["errors"]
+
     # Initial game state
     game = {
         "gameState": "over"
@@ -22,8 +25,10 @@ def handler(event, context):
     if gameExists:
         game = response["Items"][0]
         logger.info('Found existing game record {}', game)
+        responseMessage = responseMessage + "\n" + "A game record exists"
     else:
         logger.warning('No existing game record to stop')
+        responseMessage = responseMessage + "\n" + "A game record does not exist"
 
     # If the game state is "over", update the game state to "created"
     if game["gameState"] != "over":
@@ -42,8 +47,19 @@ def handler(event, context):
                 }
             )
         except ClientError as e:
+            errors = e
+            responseMessage = responseMessage + "\n" + "Error ["+e+"]"
             print("Unexpected error: %s" % e)
 
+    if gameExists and game["gameState"] == "over":
+        responseMessage = responseMessage + "\n" + "Game stopped"
+        responseMessage = responseMessage + "\n" + "Id ["+game["Id"]+"]"
+        responseMessage = responseMessage + "\n" + "Start Datetime ["+game["startDatetime"]+"]"
+        responseMessage = responseMessage + "\n" + "Game State ["+game["gameState"]+"]"
+        responseMessage = responseMessage + "\n" + "Solution ["+game["solution"]+"]"
+
+    event["data"]["errors"] = errors
     event["data"]["game"] = game
+    event["data"]["response"]["sms"] = responseMessage
 
     return event
