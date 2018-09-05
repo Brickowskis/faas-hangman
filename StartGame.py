65633e9d2b53b19c809e3a5ed6275554c0e9f865
tavttima <github@vattima.com>
2018-09-04 21:44:15 -0400

    Refactored StartGame
    * Handles multiple games in game table (but handles either a single created game or a single running game)
    * Applied refactoring for String formatting and introduced a couple helper functions
    
    TECHFEST18-23

--- StartGame.py
+++ StartGame.py
@@ -1,5 +1,6 @@
 import logging, uuid, datetime, boto3, json
 from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Attr
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -11,55 +12,65 @@ def handler(event, context):
     responseMessage = event["data"]["response"]["sms"]
     errors = event["data"]["errors"]
 
-    # Initial game state
-    game = {
-        "gameState": "over"
-    }
-
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    hangmanGame = dynamodb.Table('HangmanGame')
+    gameTable = dynamodb.Table('HangmanGame')
 
-    # Get the game from the game table
-    response = hangmanGame.scan()
-    gameExists = len(response["Items"]) == 1
-    if gameExists:
-        game = response["Items"][0]
-        logger.info('Found existing game record {}', game)
-        responseMessage = responseMessage + "\n" + "A game record exists"
-    else:
-        logger.warning('No existing game record to stop')
-        responseMessage = responseMessage + "\n" + "A game record does not exist"
+    # Get created games from the game table - should be 0 or 1
+    response = gameTable.scan(
+        FilterExpression=Attr('gameState').ne('over')
+    )
 
-    # If the game state is "created", update the game state to "running"
-    if game["gameState"] == "created":
-        # set state to over
-        game["gameState"] = 'running'
+    createdGames = list()
+    runningGames = list()
+    for game in response['Items']:
+        if game['gameState'] == 'created':
+            createdGames.append(game)
+        if game['gameState'] == 'running':
+            runningGames.append(game)
 
+    if len(createdGames) == 1:
         try:
-            # upsert the game - DynamoDB call
-            response = hangmanGame.update_item(
-                Key={
-                    'Id': game["Id"]
-                },
-                UpdateExpression='SET gameState = :st',
-                ExpressionAttributeValues={
-                    ':st': game["gameState"]
-                }
-            )
+            game = createdGames[0]
+            start_game(game, gameTable)
+            event['data']['game'] = game
+            responseMessage = append_message(responseMessage, 'Game started', game)
         except ClientError as e:
             errors = e
-            responseMessage = responseMessage + "\n" + "Error ["+e+"]"
-            print("Unexpected error: %s" % e)
+            responseMessage = append_message(responseMessage, f'Error [{e}]')
 
-    if gameExists and game["gameState"] == "running":
-        responseMessage = responseMessage + "\n" + "Game started"
-        responseMessage = responseMessage + "\n" + "Id ["+game["Id"]+"]"
-        responseMessage = responseMessage + "\n" + "Start Datetime ["+game["startDatetime"]+"]"
-        responseMessage = responseMessage + "\n" + "Game State ["+game["gameState"]+"]"
-        responseMessage = responseMessage + "\n" + "Solution ["+game["solution"]+"]"
+    elif len(runningGames) == 1:
+        event['data']['game'] = runningGames[0]
+        responseMessage = append_message(responseMessage, 'A game is already running', game)
 
-    event["data"]["errors"] = errors
-    event["data"]["game"] = game
-    event["data"]["response"]["sms"] = responseMessage
+    elif len(createdGames) == 0:
+        responseMessage = append_message(responseMessage, 'No created games exist to start')
+
+    event['data']['errors'] = errors
+    event['data']['response']['sms'] = responseMessage
 
     return event
+
+
+def start_game(game, gameTable):
+    # upsert the game - DynamoDB call
+    response = gameTable.update_item(
+        Key={
+            'Id': game['Id']
+        },
+        UpdateExpression='SET gameState = :st',
+        ExpressionAttributeValues={
+            ':st': 'running'
+        }
+    )
+
+
+def append_message(response, message, game = None):
+    if game is None:
+        return f"{response}\n{message}"
+    else:
+        return f"{response}\n{message}" + \
+               f"\nId [{game['Id']}]" + \
+               f"\nStart Datetime [{game['startDatetime']}]" + \
+               f"\nGame State [{game['gameState']}]" + \
+               f"\nSolution [{game['solution']}]"
+
