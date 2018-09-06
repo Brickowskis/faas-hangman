947b3f571eceebb27900d8e2556b11705f7e2ed3
tavttima <github@vattima.com>
2018-09-05 20:18:15 -0400

    Adds back missing files committed for StopGame
    TECHFEST18-34

---
+++ StopGameTests.py
@@ -0,0 +1,20 @@
+import unittest, json, StopGame
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_handlerWithGameOver(self):
+        #Pre-condition - HangmanGame table must be created and empty
+        input = self.loadEvent("./test/resources/test-StopGame-input-01.json")
+        event = StopGame.handler(input["input"], {})
+        print(event)
+        self.assertEqual(event["data"]["game"]["gameState"], "over")
+
+    def loadEvent(self, jsonFilename):
+        f = open(jsonFilename)
+        input = json.load(f)
+        f.close()
+        return input
+
+
+if __name__ == '__main__':
+    unittest.main()
