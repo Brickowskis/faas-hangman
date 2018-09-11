14f093e7fa5a49fff893be80364a899b26fcc213
nsstrunks <nsstrunks@gmail.com>
2018-09-11 18:59:45 -0400

    TECHFEST18-14 Completion of router and unit tests

--- Router.py
+++ Router.py
@@ -1,31 +1,36 @@
 import logging
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     to_number = event['data']['twilio']['To']
     from_number = event['data']['twilio']['From']
     body = event['data']['twilio']['Body']
-
-    errors = event["data"]["errors"]
 
     logging.info(f'Messaged directed to {to_number} from {from_number} with body {body}')
 
     action = body.split()[0].strip().lower()
     logging.info(f'Found action {action}')
 
-    if action in ['game']:
+    if action == 'game':
         event['data']['command']['type'] = 'admin'
         event['data']['command']['operation'] = 'game'
         event['data']['command']['arguments'] = body.split()[1:]
         if not isAdminNumber(from_number):
-            errors = f'{from_number} does not have access to execute admin commands.'
+            event['data']['errors'] = SystemError(f"{from_number} does not have admin access.")
             event['data']['response']['sms'] = 'You do not have permission to execute this command.'
     elif action == 'register':
         event['data']['command']['type'] = 'player'
         event['data']['command']['operation'] = 'register'
         event['data']['command']['arguments'] = body.split()[1:]
+    elif action == 'status':
+        event['data']['command']['type'] = 'player'
+        event['data']['command']['operation'] = 'status'
+        event['data']['command']['arguments'] = body.split()[1:]
+    else:
+        event['data']['command']['type'] = 'player'
+        event['data']['command']['operation'] = 'guess'
+        event['data']['command']['arguments'] = action
 
-    event['data']['errors'] = errors
     return event
 
 
--- RouterTest.py
+++ RouterTest.py
@@ -10,13 +10,88 @@ class TestIsAdminNumber(unittest.TestCase):
 
 
 class TestLambdaHandler(unittest.TestCase):
-    def test_lambdaHandler_AdminNoAccess(self):
+    def test_handler_AdminNoAccess(self):
         input = self.loadEvent("./test/resources/test-Router-Admin-Fail.json")
-        event_output = Router.lambda_handler(input['input'], None)
-        print(event_output)
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'admin')
+        self.assertEqual(event_output['data']['command']['operation'], 'game')
+        self.assertEqual(event_output['data']['command']['arguments'][0], 'stop')
+        self.assertIsInstance(event_output['data']['errors'], SystemError)
+        self.assertEqual(event_output['data']['response']['sms'], 'You do not have permission to execute this command.')
+
+    def test_lambda_AdminCreate(self):
+        input = self.loadEvent("./test/resources/test-Router-Admin-Create.json")
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'admin')
+        self.assertEqual(event_output['data']['command']['operation'], 'game')
+        self.assertEqual(event_output['data']['command']['arguments'][0], 'create')
+        self.assertEqual(event_output['data']['command']['arguments'][1], 'epiphany')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_AdminStart(self):
+        input = self.loadEvent("./test/resources/test-Router-Admin-Start.json")
+        event_output = Router.handler(input['input'], None)
+
         self.assertEqual(event_output['data']['command']['type'], 'admin')
         self.assertEqual(event_output['data']['command']['operation'], 'game')
         self.assertEqual(event_output['data']['command']['arguments'][0], 'start')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_AdminStop(self):
+        input = self.loadEvent("./test/resources/test-Router-Admin-Stop.json")
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'admin')
+        self.assertEqual(event_output['data']['command']['operation'], 'game')
+        self.assertEqual(event_output['data']['command']['arguments'][0], 'stop')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_AdminStatus(self):
+        input = self.loadEvent("./test/resources/test-Router-Admin-Status.json")
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'admin')
+        self.assertEqual(event_output['data']['command']['operation'], 'game')
+        self.assertEqual(event_output['data']['command']['arguments'][0], 'status')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_PlayerRegister(self):
+        input = self.loadEvent("./test/resources/test-Router-Player-Register.json")
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'player')
+        self.assertEqual(event_output['data']['command']['operation'], 'register')
+        self.assertEqual(event_output['data']['command']['arguments'][0], 'nessy')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_PlayerStatus(self):
+        input = self.loadEvent("./test/resources/test-Router-Player-Status.json")
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'player')
+        self.assertEqual(event_output['data']['command']['operation'], 'status')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_PlayerGuessLetter(self):
+        input = self.loadEvent("./test/resources/test-Router-Player-GuessLetter.json")
+        event_output = Router.handler(input['input'], None)
+
+        self.assertEqual(event_output['data']['command']['type'], 'player')
+        self.assertEqual(event_output['data']['command']['operation'], 'guess')
+        self.assertEqual(event_output['data']['command']['arguments'], 'a')
+        self.assertEqual(event_output['data']['errors'], {})
+
+    def test_lambda_PlayerGuessWord(self):
+        input = self.loadEvent("./test/resources/test-Router-Player-GuessWord.json")
+        event_output = Router.handler(input['input'], None)
+        print(event_output)
+
+        self.assertEqual(event_output['data']['command']['type'], 'player')
+        self.assertEqual(event_output['data']['command']['operation'], 'guess')
+        self.assertEqual(event_output['data']['command']['arguments'], 'epiphany')
+        self.assertEqual(event_output['data']['errors'], {})
 
     def loadEvent(self, jsonFilename):
         f = open(jsonFilename)
