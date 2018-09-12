a7259485444f24a70a956e47702f5dae331e5dc7
John Killmer <killmer.john@gmail.com>
2018-09-11 23:25:06 -0400

    TECHFEST18-24 Unhardcoded the phone number

--- register.py
+++ register.py
@@ -2,18 +2,23 @@ import logging, boto3
 
 # https://support.twilio.com/hc/en-us/articles/223181468-How-do-I-Add-a-Line-Break-in-my-SMS-or-MMS-Message-
 CRLB = "%0a"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
+
+    logger.info('Handling event {} - context {}', event, context)
+
     player_name = event['data']['command']['arguments'][0]
-    #logging.info(f'Received registration request for {from_number}')
+    phone_number = event['data']['twilio']['From']
 
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-    playerTable = dynamodb.Table('HangmanPlayer')
+    player_table = dynamodb.Table('HangmanPlayer')
 
-    playerTable.put_item(
+    player_table.put_item(
         Item={
-            'phoneNumber': '6105551234',
+            'phoneNumber': phone_number,
             'playerState': 'REGISTERED',
             'playerName': player_name
         }
