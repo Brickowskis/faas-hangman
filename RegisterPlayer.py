c431f3cfd40ba365bdea3ea93f0df4ad9f63d4ae
John Killmer <killmer.john@gmail.com>
2018-10-03 18:28:04 -0400

    TECHFEST18-24 Adding the running game id to the player register

--- RegisterPlayer.py
+++ RegisterPlayer.py
@@ -1,5 +1,7 @@
 import logging, boto3
 
+from boto3.dynamodb.conditions import Attr
+
 # https://support.twilio.com/hc/en-us/articles/223181468-How-do-I-Add-a-Line-Break-in-my-SMS-or-MMS-Message-
 CRLB = "%0a"
 logger = logging.getLogger()
@@ -7,18 +9,31 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-
     logger.info('Handling event {} - context {}', event, context)
 
     player_name = event['data']['command']['arguments'][0]
     phone_number = event['data']['twilio']['From']
 
     dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+    game_table = dynamodb.Table('HangmanGame')
     player_table = dynamodb.Table('HangmanPlayer')
 
+    # Get the running game
+    response = game_table.scan(
+        FilterExpression=Attr('gameState').ne('over')
+    )
+    running_games = list()
+    for game in response['Items']:
+        if game['gameState'] == 'running':
+            running_games.append(game)
+
+    # Register the player for the running game
+    game = running_games[0]
     player_table.put_item(
         Item={
             'phoneNumber': phone_number,
+            'gameId': game['Id'],
             'playerState': 'REGISTERED',
             'playerName': player_name
         }
