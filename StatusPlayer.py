0c23e692c1ce3023ed3249466e36b40afd021d01
John Killmer <killmer.john@gmail.com>
2018-10-03 18:13:55 -0400

    TECHFEST18-35 Retrieving the player state

--- StatusPlayer.py
+++ StatusPlayer.py
@@ -1,28 +1,41 @@
-import logging
+import logging, boto3
 
 # https://support.twilio.com/hc/en-us/articles/223181468-How-do-I-Add-a-Line-Break-in-my-SMS-or-MMS-Message-
 CRLB = "%0a"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event, context):
-    to_number = event['To']
-    from_number = event['From']
-    logging.info(f'Received status request for {from_number}')
+def handler(event, context):
+    logger.info('Handling event {} - context {}', event, context)
+    phone_number = event['data']['twilio']['From']
 
-    return build_response(hangman()+f'{CRLB}{CRLB}'+letter_available())
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    player_table = dynamodb.Table('HangmanPlayer')
+    response = player_table.get_item(
+        Key={
+            'phoneNumber': phone_number,
+            'gameId': 'e97ceae3-dd84-4042-b3d8-57ceb54b6e64'
+        }
+    )
+    item = response['Item']
+    print(item)
+
+    #phoneNumber/String
+    #guesses
+    #playerState
+    #lastGuessDatetime
+    #playerName
+
+    #lives remaining
+    #guess attempted
+    #current solve state
+
+    return build_response(f'{CRLB}{CRLB}'+letter_available())
 
 
 def build_response(body):
     return f'<?xml version="1.0" encoding="UTF-8"?><Response><Sms>{body}</Sms></Response>'
-
-#╔═══╗
-#    ║
-#    ║
-#    ║
-#════╝
-
-def hangman():
-    return f'╔═══╗{CRLB}    ║{CRLB}    ║{CRLB}    ║{CRLB}════╝'
 
 def letter_available():
     return f'A B C D E F G H I J L M N{CRLB}O P Q R S T U V W X Y Z'
---
+++ StatusPlayerTests.py
@@ -0,0 +1,22 @@
+import StatusPlayer
+import json
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_statusPlayer(self):
+        input = self.loadEvent("./test/resources/test-StatusPlayer-input-01.json")
+        event = StatusPlayer.handler(input["input"], {})
+        print(event)
+        #self.assertEqual(event["data"]["game"]["gameState"], "running")
+
+    def loadEvent(self, jsonFilename):
+        f = open(jsonFilename)
+        input = json.load(f)
+        f.close()
+        return input
+
+
+if __name__ == '__main__':
+    unittest.main()
